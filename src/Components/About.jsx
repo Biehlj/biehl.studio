d874cbc5a174e3afbed14cc7b5e4dda69a9f2d50
John B <128255094+Biehlj@users.noreply.github.com>
2023-09-15 00:38:30 +0000

    retry

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Information Systems student studying at Southwestern Illinois College. I enjoy utilizing the skills I've learned to create products for myself and others.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Cybersecurity",
+  "Application development",
+  "DevSecOps practices",
+  "Web Design",
+  "Vulnerability Analysis",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "The best way to predict the future is to create it. - Abraham Lincoln";
 
 const About = () => {
   return (
---
+++ src/Components/Education.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const educationList = [
+  {
+    school: "Purdue University",
+    degree: "Bachelor of Science in Computer and Information Technology",
+    description:
+      "Attended Purdue University for a Bachelor of Science in Computer and Information Technology. Graduated in 2020.",
+  },
+  {
+    school: "Southwestern Illinois",
+    degree: "Associate of Applied Science in Computer Information Systems",
+    description:
+      "Attending Southwestern Illinois for an Associate of Applied Science in Computer Information Systems.",
+  },
+];
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2 style={{ textAlign: "center" }}> Education</h2>
+      <div className="container">
+        {educationList.map((education) => (
+          <div className="box" key={education.school}>
+            <div className="flex-content">
+              <a href={education.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{education.school}</h3>
+              </a>
+              <h3>{education.degree}</h3>
+              <p className="small">{education.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Education;
