00edca87d82d6f2fb77825d646015f21f8733b01
John B <128255094+Biehlj@users.noreply.github.com>
2023-10-10 21:04:39 +0000

    portfolio update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,26 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "SWIC Projects",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A simple web app that takes a name and returns it in a variety of formats. Built with JavaScript, HTML, and CSS.",
+    url: "https://github.com/Biehlj/School-Projects",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Home Lab Terraform",
+    description: "Created a Terraform file to deploy resources to my home lab utilizing Terraform",
+    url: "https://github.com/Biehl-Net/Biehl-net",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created from Microsoft's resume workshop and deployed. Includes my experience and design abilities.",
+    url: "https://github.com/Biehlj/resume",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "My GitHub",
+    description: "My GitHub page with all of my projects.",
+    url: "https://github.com/Biehlj",
   },
 ];
 
